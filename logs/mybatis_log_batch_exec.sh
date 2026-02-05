fdcaa1dbdd1c90d92f15ceba665b02de52af765a
smartdiy <93225234+smartdiy@users.noreply.github.com>
2026-02-05 14:15:25 -0500

    Refactor parameter processing in mybatis_log_batch_exec.sh

--- logs/mybatis_log_batch_exec.sh
+++ logs/mybatis_log_batch_exec.sh
@@ -15,28 +15,18 @@ function trim(s) { gsub(/^[ \t\r\n]+|[ \t\r\n]+$/, "", s); return s }
 function is_number(s) { return s ~ /^-?[0-9]+(\.[0-9]+)?([eE]-?[0-9]+)?$/ }
 function normalize(s) { return (s ~ /[eE]/) ? (s+0) : s }
 
-# replace single quotes inside strings
-function escape_single_quotes(s,   i, c, out) {
-    out=""
-    for(i=1;i<=length(s);i++){
-        c=substr(s,i,1)
-        if(c=="'") out = out "''"
-        else out = out c
-    }
-    return out
-}
-
 BEGIN { idx=1 }
 
 /Preparing:/ {
   sql = $0
-  sub(/.*Preparing:[[:space:]]*/,"",sql)
+  sub(/.*Preparing:[[:space:]]*/, "", sql)
   gsub(/[{}]/,"",sql)
   gsub(/\([[:space:]]*\?[[:space:]]*(,[[:space:]]*\?)*[[:space:]]*\)/,"",sql)
   sql=trim(sql)
   next
 }
 
+# Process Parameters
 /Parameters:/ && sql != "" {
   file = sprintf("%s/exec_%03d.sql", outdir, idx++)
   line = $0
@@ -44,20 +34,23 @@ BEGIN { idx=1 }
   n = split(line, arr, ", ")
 
   vals=""
-  for(i=1;i<=n;i++){
+  for(i=1;i<=n;i++) {
     raw = trim(arr[i])
     val = raw
-    # remove (Type)
+
+    # remove type annotation
     sub(/\([^)]+\)$/,"",val)
     val = trim(val)
 
-    if(tolower(val)=="null"){
-        val_out="NULL"
-    } else if(is_number(val)){
-        val_out=normalize(val)
+    if(tolower(val) == "null") {
+      val_out = "NULL"
+    } else if(is_number(val)) {
+      val_out = normalize(val)
     } else {
-        val_clean = escape_single_quotes(val)
-        val_out = sprintf("%c%s%c", 39, val_clean, 39)
+      # Escape single quotes correctly
+      gsub(/'/, "''", val)
+      # wrap string in single quotes
+      val_out = sprintf("%c%s%c", 39, val, 39)
     }
 
     vals = vals (i==1?"":", ") val_out
